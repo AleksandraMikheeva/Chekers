01f347e1bbd7f84895d0bb56fe07941d34b3de08
Mikheeva128 <im89118350092@gmail.com>
2021-10-28 03:15:16 +0300

    created cell class

---
+++ src/Cell.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Cell {
+    public enum State {BLACK, WHITE, BLANK}
+
+    private int[] mPosition;
+    private State mState;
+
+    public Cell(int[] mPosition, State mState) {
+        this.mPosition = mPosition;
+        this.mState = mState;
+    }
+
+    //    public int[] getPosition() { return mPosition; }
+    public int[] getPosition() {
+        return Arrays.copyOf(mPosition, mPosition.length);
+    }
+
+    public void setPosition(int[] position) {
+        mPosition = position;
+    }
+
+    public State getState() {
+        return mState;
+    }
+
+    public void setState(State state) {
+        mState = state;
+    }
+
+
+}
